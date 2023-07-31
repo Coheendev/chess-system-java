fc8d1447675c620632b9c8bc5381a80de71cff49
Luiz Fernando <devcohenrpg@gmail.com>
2023-07-31 13:52:43 -0300

    Check logic

--- src/app/UI.java
+++ src/app/UI.java
@@ -34,8 +34,8 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
-    public static void clearScreen(){
-        System.out.print("\033[H\033[2j");
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
         System.out.flush();
     }
     public static ChessPosition readChessPosition(Scanner sc){
@@ -58,6 +58,9 @@ public class UI {
         System.out.println();
         System.out.println("Turn: " + chessMatch.getTurn());
         System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+        if (chessMatch.getCheck()){
+            System.out.println("CHECK!!");
+        }
     }
 
 
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -9,11 +9,13 @@ import chess.pieces.Rook;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ChessMatch {
     private Board board;
     private int turn;
     private Color currentPlayer;
+    private boolean check;
 
     private List<Piece> piecesOnTheBoard = new ArrayList<>();
     private List<Piece> capturedPieces = new ArrayList<>();
@@ -32,6 +34,7 @@ public class ChessMatch {
     public Color getCurrentPlayer(){
         return currentPlayer;
     }
+    public boolean getCheck (){ return check;}
 
     public ChessPiece[][] getPieces() {
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
@@ -54,6 +57,14 @@ public class ChessMatch {
         validateSourcePosition(source);
         validateTargetPosisiton(source, target);
         Piece capturedPiece = makeMove(source, target);
+
+        if (testCheck(currentPlayer)){
+            undoMove(source,target, capturedPiece);
+            throw new ChessException("You can't put yourself in check");
+        }
+
+        check = (testCheck(opponent(currentPlayer))) ? true : false;
+
         nextTurn();
         return (ChessPiece) capturedPiece;
     }
@@ -66,6 +77,16 @@ public class ChessMatch {
             capturedPieces.add(capturePiece);
         }
         return  capturePiece;
+    }
+    private void undoMove (Position source, Position target, Piece capturePiece){
+        Piece p = board.removePiece(target);
+        board.placePiece(p, source);
+
+        if (capturePiece != null){
+            board.placePiece(capturePiece, target);
+            capturedPieces.remove(capturePiece);
+            piecesOnTheBoard.add(capturePiece);
+        }
     }
     private void validateSourcePosition (Position position){
         if (!board.thereIsAPiece(position)){
@@ -87,6 +108,32 @@ public class ChessMatch {
     private void nextTurn(){
         turn++;
         currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
+    }
+
+    private Color opponent(Color color){
+        return (color == Color.WHITE)? Color.BLACK : Color.WHITE;
+    }
+
+    private ChessPiece king(Color color){
+        List<Piece> list = piecesOnTheBoard.stream().filter(x-> ((ChessPiece)x).getColor() == color).collect(Collectors.toList());
+        for (Piece p : list){
+            if (p instanceof King){
+                return (ChessPiece) p;
+            }
+        }
+        throw new IllegalStateException("There is no " + color + "king on the board");
+    }
+
+    private boolean testCheck (Color color){
+        Position kingPosition = king(color).getChessPosition().toPosition();
+        List<Piece>opponentPieces = piecesOnTheBoard.stream().filter(x-> ((ChessPiece)x).getColor() == opponent(color)).collect(Collectors.toList());
+        for (Piece p : opponentPieces){
+            boolean [][] mat = p.possibleMoves();
+            if (mat[kingPosition.getRow()][kingPosition.getColumn()]){
+                return true;
+            }
+        }
+        return false;
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece){
